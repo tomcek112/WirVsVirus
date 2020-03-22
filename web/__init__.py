0246e42ae8522844ed2db680371576aee4c2119c
deirdrebringas <deirdre.bringas@freeletics.com>
2020-03-22 13:40:02 +0100

    Add request params for date

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import urllib.parse as urlparse
 import psycopg2
+import time
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -14,7 +15,24 @@ def hello_world():
 def observations():
     con = _getDbConnection()
     cur = con.cursor()
-    cur.execute("SELECT observations.id, observations.camera_id, observations.count, observations.date, cameras.long, cameras.lat FROM observations INNER JOIN cameras ON observations.camera_id=cameras.camera_id")
+    if request.args:
+        args = request.args
+        date_from = time.ctime(args["from"])
+        date_to = time.ctime(args["to"])
+        cur.execute("""
+        SELECT observations.id, observations.camera_id, observations.count, observations.date, cameras.long, cameras.lat
+        FROM observations
+        INNER JOIN cameras
+        ON observations.camera_id=cameras.camera_id
+        WHERE observations.date >= (%s)::date
+        AND observations.date <= (%s)::date
+        """, (date_from, date_to))
+    else:
+        cur.execute("""
+        SELECT observations.id, observations.camera_id, observations.count, observations.date, cameras.long, cameras.lat
+        FROM observations
+        INNER JOIN cameras
+        ON observations.camera_id=cameras.camera_id""")
     obs = []
     for row in cur.fetchall():
         obs.append({
@@ -25,6 +43,8 @@ def observations():
             'long': row[4],
             'lat': row[5],
         })
+    cur.close()
+    con.close()
     return jsonify({'observations': obs})
 
 def _getDbConnection():
